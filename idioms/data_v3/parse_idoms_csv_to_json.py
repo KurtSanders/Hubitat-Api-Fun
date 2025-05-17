81541ad9df74a135e8647cf36b0ddd7925882060
Kurt Sanders <kurt@kurtsanders.com>
2025-05-17 18:50:55 -0400

    Update parse_idoms_csv_to_json.py

--- idioms/data_v3/parse_idoms_csv_to_json.py
+++ idioms/data_v3/parse_idoms_csv_to_json.py
@@ -3,18 +3,19 @@ import csv
 
 infilename = "/Users/sandeke/PycharmProjects/Hubitat-Fun/idioms/data_v3/english_idioms.csv"
 outfilename = "/Users/sandeke/Downloads/Idioms.json"
-d = []
+d = {'Idioms': []}
 
 # Write dictionary to a JSON file
 def write_dict_to_json(data, fn):
     with open(fn, 'w') as outfile:
-        json.dump(data, outfile, indent=0, ensure_ascii=False)  # indent for better readability
+        json.dump(data, outfile, separators=(',', ':'), indent=0, ensure_ascii=False)  # indent for better readability
 
 reader = csv.reader(open(infilename, 'r'))
 id = 0
 with open(infilename) as infile:
     for i in csv.DictReader(infile):
-        d.append(dict(i))
-print (f"Writing final dictionary to {outfilename}")
+        id += 1
+        d['Idioms'].append(dict(i))
+print (f"Writing {id} records to final dictionary to {outfilename}")
 write_dict_to_json(d,outfilename)
 
